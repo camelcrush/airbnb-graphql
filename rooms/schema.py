9bcf446de7eadb8fe8c890d342ca4c66e48c0d7a
camelcrush <dkxm0927@naver.com>
2021-11-20 16:44:31 +0900

    Separate Room types & schema

--- rooms/schema.py
+++ rooms/schema.py
@@ -1,17 +1,6 @@
 import graphene
-from graphene_django import DjangoObjectType
+from .types import RoomListResponse
 from .models import Room
-
-
-class RoomType(DjangoObjectType):
-    class Meta:
-        model = Room
-
-
-class RoomListResponse(graphene.ObjectType):
-
-    arr = graphene.List(RoomType)
-    total = graphene.Int()
 
 
 class Query(object):
